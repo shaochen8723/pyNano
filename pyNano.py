7d74ac7c565949bde7f8e7e167a82d7ad82123be
shaochen8723 <shaochen8723@gmail.com>
2015-07-22 08:39:41 -0500

    7/22/15

---
+++ pyNano.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python -tt
+# Chen Shao
+# Module to predict strain energy in PMMA/graphene nanocomposites
+
+import 
+
+# global parameters
+UTS = 10 #GPa UTS for multilayer graphene
+dg = 0.34 #nm interlayer distance between graphene sheets
+
+def getMLGStrength():
+  
+  return
+
+def
